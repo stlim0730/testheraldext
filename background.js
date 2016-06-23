41ed2756469edc32a4903a34cafac252229be55c
Seongtaek Lim <slim@imac-25.propublica.local>
2016-06-23 10:58:59 -0400

    just minor indentation

--- background.js
+++ background.js
@@ -1,49 +1,51 @@
 var gport = null;
+
 chrome.runtime.onConnect.addListener(function connectListener(port) {
   gport = port;
-  console.log("CONNECTED",port.name);
+  console.log('CONNECTED', port.name);
+
   // chrome.runtime.onConnect.removeListener(connectListener);
-  // console.assert(port.name == "optimisely");
-  port.onMessage.addListener(function(msg) {
+  // console.assert(port.name == 'optimisely');
+  port.onMessage.addListener(function (msg) {
     
-    
-    if(msg.state === "init"){
+    if(msg.state === 'init') {
       injectScript( chrome.extension.getURL('optimisely-sniffer.js'), 'body');
-      window.addEventListener("message",injectedMessageHandler,false);
-      port.postMessage({state: "doing it"});
+      window.addEventListener('message', injectedMessageHandler, false);
+      port.postMessage({state: 'doing it'});
     }
-    else if(msg.state == 'eval'){
+    else if(msg.state == 'eval') {
       console.log(msg.id);
       // var msg ={type:'variation',data: optimizely['allExperiments']};
-      window.postMessage(msg, "*");
+      window.postMessage(msg, '*');
     }
+
   });
 
   port.onDisconnect.removeListener(function(){
-    console.log("removing listener"); 
-    window.removeEventListener("message",injectedMessageHandler,false);
+    console.log('removing listener'); 
+    window.removeEventListener('message',injectedMessageHandler,false);
   })
 });
 
-
 function injectScript(file, node) {
-    var th = document.getElementsByTagName(node)[0];
-    var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('src', file);
-    (document.head||document.documentElement).appendChild(s);
+  var th = document.getElementsByTagName(node)[0];
+  var s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('src', file);
+  (document.head||document.documentElement).appendChild(s);
 }
 
 // function onConnectListener()
 
-function injectedMessageHandler(event){
+function injectedMessageHandler(event) {
   // console.log(event.data);
-  if(gport === null)console.log("No active port");
-  if(event.data.type === 'no-optimisely'){
+  if(gport === null) console.log('No active port');
+
+  if(event.data.type === 'no-optimisely') {
     gport.postMessage({state: 'no-optimisely'});
   }
-  else if(event.data.type === 'variation' ){
-    console.log("I am here");
+  else if(event.data.type === 'variation') {
+    console.log('I am here');
     gport.postMessage({state: event.data.type,experiments:event.data.experiments,variations:event.data.variations});
   }
 }
