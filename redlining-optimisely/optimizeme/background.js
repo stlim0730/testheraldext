5f19b94f5aa2bcdd04147a1f79c63a7fa8815854
Seongtaek Lim <slim@imac-25.propublica.local>
2016-06-24 18:21:00 -0400

    in progress: refactoring

--- redlining-optimisely/optimizeme/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(
-  function(message, sender, sendResponse) {
-    chrome.pageAction.show(sender.tab.id)
-  });
-
-chrome.pageAction.onClicked.addListener(
-  function (tab) {
-    chrome.tabs.sendMessage(tab.id, "");
-});
