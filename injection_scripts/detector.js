b1052b44ff8ff417f29176afa5d6ac3b663ec689
Seongtaek Lim <slim@imac-25.propublica.local>
2016-08-05 16:36:45 -0400

    changed run_at in manifest; detection starts window.onload

--- injection_scripts/detector.js
+++ injection_scripts/detector.js
@@ -1,12 +1,16 @@
-if (window.optimizely) {
-  window.postMessage({
-    sender: 'injection',
-    receiver: 'content',
-    event: 'found optimizely',
-    target: JSON.stringify(window.optimizely)
-    // Message event sometimes fails with nested objects for some reason; serialize them.
-  }, '*');
-}
-else {
-  console.info('There is no optimizely.');
-}
+var detect = function() {
+  if (window.optimizely) {
+    window.postMessage({
+      sender: 'injection',
+      receiver: 'content',
+      event: 'found optimizely',
+      target: JSON.stringify(window.optimizely)
+      // Message event sometimes fails with nested objects for some reason; serialize them.
+    }, '*');
+  }
+  else {
+    console.info('There is no optimizely.');
+  }
+};
+
+window.onload = detect;
